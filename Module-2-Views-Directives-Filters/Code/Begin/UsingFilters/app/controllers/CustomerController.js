9627c1c1294844ceda48790f8df54b810bf1e76c
Ishan <ishan@mtlbigger.com>
2022-04-14 15:27:17 +0530

    Using Option 3 with controller-as directive

--- Module-2-Views-Directives-Filters/Code/Begin/UsingFilters/app/controllers/CustomerController.js
+++ Module-2-Views-Directives-Filters/Code/Begin/UsingFilters/app/controllers/CustomerController.js
@@ -1,5 +1,6 @@
 (function(){
-    const custCtrl = (scope)=>{
+    const custCtrl = function(){
+        scope = this;
         scope.sortVar = 'name';
         scope.reverse = false;
         scope.customers=customers=[{joined: '2000-12-02', name:'John', city:'Chandler', orderTotal: 9.9956}, {joined: '1965-01-25',name:'Zed', city:'Las Vegas', orderTotal: 19.99},{joined: '1944-06-15',name:'Tina', city:'New York', orderTotal:44.99}, {joined: '1995-03-28',name:'Dave', city:'Seattle', orderTotal:101.50}]
@@ -8,6 +9,5 @@
             scope.reverse = !scope.reverse;
         }
     }
-    custCtrl.$inject = ['$scope'];
     angular.module('customersApp').controller('CustomersController',custCtrl)
 })();
