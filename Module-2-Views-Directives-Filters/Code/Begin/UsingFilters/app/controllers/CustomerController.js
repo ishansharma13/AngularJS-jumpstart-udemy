4088f2212ca2e9be5f85dd90bf8c7b34be1dbba3
Ishan <ishan@mtlbigger.com>
2022-04-14 14:35:26 +0530

    aliasing the default  useful when dealing with nested scopes

--- Module-2-Views-Directives-Filters/Code/Begin/UsingFilters/app/controllers/CustomerController.js
+++ Module-2-Views-Directives-Filters/Code/Begin/UsingFilters/app/controllers/CustomerController.js
@@ -1,9 +1,9 @@
-app.controller('CustomersController', function ($scope){
-    $scope.sortVar = 'name';
-    $scope.reverse = false;
-    $scope.customers=customers=[{joined: '2000-12-02', name:'John', city:'Chandler', orderTotal: 9.9956}, {joined: '1965-01-25',name:'Zed', city:'Las Vegas', orderTotal: 19.99},{joined: '1944-06-15',name:'Tina', city:'New York', orderTotal:44.99}, {joined: '1995-03-28',name:'Dave', city:'Seattle', orderTotal:101.50}]
-    $scope.doSort = function(propColumn){
-        $scope.sortVar = propColumn;
-        $scope.reverse = !$scope.reverse;
+app.controller('CustomersController', function (){
+    this.sortVar = 'name';
+    this.reverse = false;
+    this.customers=customers=[{joined: '2000-12-02', name:'John', city:'Chandler', orderTotal: 9.9956}, {joined: '1965-01-25',name:'Zed', city:'Las Vegas', orderTotal: 19.99},{joined: '1944-06-15',name:'Tina', city:'New York', orderTotal:44.99}, {joined: '1995-03-28',name:'Dave', city:'Seattle', orderTotal:101.50}]
+    this.doSort = function(propColumn){
+        this.sortVar = propColumn;
+        this.reverse = !this.reverse;
     }
 })
