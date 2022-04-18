7d63ecf81b03e9371b6ee7c5e1f80b942373a285
Ishan <ishan@mtlbigger.com>
2022-04-18 15:48:51 +0530

    Added ordersController and related files

--- Module-4-Routing/Code/Begin/WorkingWithRoutes/app/app.js
+++ Module-4-Routing/Code/Begin/WorkingWithRoutes/app/app.js
@@ -7,7 +7,12 @@
             controller: 'CustomersController',
             templateUrl: 'app/views/customers.html'
 
-        }).otherwise({redirectTo: '/'});
+        })
+        .when('/orders/:customerId',{
+            controller: 'OrdersController',
+            templateUrl: 'app/views/orders.html'
+        })
+        .otherwise({redirectTo: '/'});
     })
     
 }());
